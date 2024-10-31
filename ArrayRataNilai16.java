933984e6beb8d30f7d51f61ac73b55988e3eb513
FattahulAlim <fathalim1512@gmail.com>
2024-10-31 08:47:15 +0700

    Some changes

--- ArrayRataNilai16.java
+++ ArrayRataNilai16.java
@@ -5,16 +5,29 @@ public class ArrayRataNilai16 {
         Scanner sc = new Scanner(System.in);
 
         int[] nilaiMhs = new int[10];
-        double total = 0, rata2;
+        double totalLls = 0, rataTdkLls=0,rataLulus=0, totalTdkLls=0;
+        int n, lulus=0, tdkLulus=0;
 
-        for (int i = 0; i<nilaiMhs.length; i++) {
+        System.out.print("Berapa jumlah mahasiswa anda: ");
+        n =sc.nextInt();
+        
+        for (int i = 0; i<n; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
-        for (int i = 0; i<nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-            rata2 = total/nilaiMhs.length;
-            System.out.println("Rata-rata nilai = " +rata2);
+        for (int i = 0; i<n; i++) {
+            if (nilaiMhs[i] > 70) {
+                totalLls += nilaiMhs[i];
+                lulus++;
+            } else {
+                totalTdkLls += nilaiMhs[i];
+                tdkLulus++;
+            }
+        } rataLulus += totalLls/lulus;
+        rataTdkLls +=totalTdkLls/tdkLulus;
+
+        System.out.println("Rata rata nilai lulus = " + rataLulus);
+        System.out.println("Rata rata nilai tidak lulus = " + rataTdkLls);
+       
     }
 }
